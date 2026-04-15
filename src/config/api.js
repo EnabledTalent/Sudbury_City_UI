fdcec1dfb9ea8e85e7ce8f3eab47c24a1425064d
Anurag005-MS <101936603+Anurag005-MS@users.noreply.github.com>
2026-04-15 11:04:13 +0530

    render change

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 // Base URL for all endpoints post login (business endpoints)
-export const BUSINESS_BASE_URL = "http://localhost:8097";
+export const BUSINESS_BASE_URL = "https://sudburry-java-service.onrender.com";
 
 // Base URL for login/register/signup/logout (API Gateway)
-const BASE_URL = "http://localhost:8091";
+const BASE_URL = "https://sudbury-java-service.onrender.com";
 
 // Auth service URL (same as BASE_URL for login/signup/logout)
 export const AUTH_BASE_URL = "https://sudbury-auth-service.onrender.com";
