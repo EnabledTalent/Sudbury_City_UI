d43f282dd20ff60fea0a557e9dbfe4bbdeab9aea
Anurag005-MS <101936603+Anurag005-MS@users.noreply.github.com>
2026-04-15 15:27:59 +0530

    fix for login

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 export const BUSINESS_BASE_URL = "https://sudburry-java-service.onrender.com";
 
 // Base URL for login/register/signup/logout (API Gateway)
-const BASE_URL = "https://sudbury-java-service.onrender.com";
+const BASE_URL = "https://sudbury-auth-service.onrender.com";
 
 // Auth service URL (same as BASE_URL for login/signup/logout)
 export const AUTH_BASE_URL = "https://sudbury-auth-service.onrender.com";
